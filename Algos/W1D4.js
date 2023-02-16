89e833d03c104f469e6532d4651b7d4d081e6228
WinterP <winterp.dev@gmail.com>
2023-02-16 16:34:36 -0600

    Update Day 4

---
+++ Algos/W1D4.js
@@ -0,0 +1,20 @@
+
+// Functions
+// Functions are Asynchronous
+// (Out of sync with the rest of the code)
+
+var x = 5;
+
+function setX(newValue) {
+    x = newValue;
+}
+
+console.log(newValue);
+
+console.log(x);
+setX(15);
+console.log(x);
+
+
+
+
